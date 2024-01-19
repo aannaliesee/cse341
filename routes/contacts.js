b1c3ef3644a1a44319e8a7a072a7306e1444d3b2
Annaliese Mits <annaliese.mits@gmail.com>
2024-01-19 12:45:39 +1100

    Testing Render

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,31 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// Your route definitions will go here
-const Contact = require('../models/Contact'); // Assuming you have a Contact model
+const contactsController = require('../controllers/contacts');
 
-//route to handle the GET request to retrieve all contacts
-router.get('/', async (req, res) => {
-  try {
-    const contacts = await Contact.find();
-    res.json(contacts);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get('/', contactsController.getAll);
 
-//route to handle GET request to retrieve a single contact by ID
-router.get('/:id', async (req, res) => {
-    try {
-      const contact = await Contact.findById(req.params.id);
-      if (!contact) {
-        return res.status(404).json({ message: 'Contact not found' });
-      }
-      res.json(contact);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-  
+router.get('/:id', contactsController.getSingle);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
-
-const mongoose = require('mongoose');
-require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const contactsRoute = require('./routes/contacts');
-app.use('/contacts', contactsRoute);
-
-app.use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log('Server running on port 3000');
+});
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
