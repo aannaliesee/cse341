d53b315fc4838fb80439d78780b0c4638c9ba71c
Annaliese Mits <annaliese.mits@gmail.com>
2024-01-28 10:59:30 +1100

    Updated routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -25,7 +25,31 @@ const getSingle = async (req, res, next) => {
 };
 
 //LESSON 3
+
 const createContact = async (req, res) => {
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await mongodb.getDb().db(Lesson2).collection('contacts').insertOne(contact);
+
+    if (response.acknowledged) {
+      res.status(201).json({ id: response.insertedId });
+    } else {
+      res.status(500).json({ error: 'Some error occurred while creating the contact.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+/*const createContact = async (req, res) => {
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -33,13 +57,15 @@ const createContact = async (req, res) => {
     favoriteColor: req.body.favoriteColor,
     birthday: req.body.birthday
   };
-  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  const response = await mongodb.getDb().db(Lesson2).collection('contacts').insertOne(contact);
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
-  }
+  } 
 };
+*/
+
 
 const updateContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
@@ -53,9 +79,10 @@ const updateContact = async (req, res) => {
   };
   const response = await mongodb
     .getDb()
-    .db()
+    .db('Lesson2')
     .collection('contacts')
-    .replaceOne({ _id: userId }, contact);
+    //changed to updateOne and $set: contact
+    .updateOne({ _id: userId }, { $set: contact });
   console.log(response);
   if (response.modifiedCount > 0) {
     res.status(204).send();
@@ -66,7 +93,7 @@ const updateContact = async (req, res) => {
 
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  const response = await mongodb.getDb().db(Lesson2).collection('contacts').remove({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
--- routes/contacts.js
+++ routes/contacts.js
@@ -5,5 +5,11 @@ const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getSingle);
+//Lesoon3 Create, Update, Delete contacts
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
