022439f6406c406b3232d6276601a38e9274c3d9
Annaliese Mits <annaliese.mits@gmail.com>
2024-01-28 13:33:33 +1100

    final try

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -62,7 +62,7 @@ const createContact = async (req, res) => {
     favoriteColor: req.body.favoriteColor,
     birthday: req.body.birthday
   };
-  const response = await mongodb.getDb().db(Lesson2).collection('contacts').insertOne(contact);
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
@@ -97,7 +97,7 @@ const updateContact = async (req, res) => {
 
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db(Lesson2).collection('contacts').remove({ _id: userId }, true);
+  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
