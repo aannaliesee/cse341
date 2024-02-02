22388914e209229f87f8ad2f6bed2a42ffba8d73
Annaliese Mits <annaliese.mits@gmail.com>
2024-02-02 11:58:55 +1100

    Swagger Setup

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const routes = require('express').Router();
 const myController = require('../controllers');
 const router = express.Router();
 
+routes.use('/', require('./swagger'));
 routes.get('/', myController.stevenFunction);
 routes.get('/anna', myController.annaFunction);
 
--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const port = process.env.PORT || 3000;
 contactsRoute = require('./routes/contacts');
 app.use('/contacts', contactsRoute);
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
   })
   .use('/', require('./routes'));
